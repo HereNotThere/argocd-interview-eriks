0bcef142dc51c450f7c05e3a3e4e6071e85c2160
Kerem Kazan <kerem.kazan@gmail.com>
2025-02-12 17:01:01 -0500

    created db config script

---
+++ charts/notification-service/templates/db-config/src/index.ts
@@ -0,0 +1,168 @@
+import { Client, DatabaseError } from 'pg'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    DB_HOST: z.string(),
+    DB_DATABASE: z.string(),
+    DB_ROOT_USER: z.string(),
+    DB_ROOT_PASSWORD: z.string(),
+    DB_NOTIFICATION_SERVICE_USER: z.string(),
+    DB_NOTIFICATION_SERVICE_USER_PASSWORD: z.string(),
+})
+
+const env = envSchema.parse(process.env)
+
+function isDBAlreadyExistsError(e: unknown) {
+    return e instanceof DatabaseError && (e.code == '42P04' || e.code == '23505')
+}
+
+const createSchema = async () => {
+    const schemaName = 'notification_service'
+    // create schema if not exists
+    console.log('creating schema')
+
+    let error
+
+    const pgClient = new Client({
+        host: env.DB_HOST,
+        database: env.DB_DATABASE,
+        password: env.DB_NOTIFICATION_SERVICE_USER_PASSWORD,
+        user: env.DB_NOTIFICATION_SERVICE_USER,
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    })
+
+    try {
+        console.log('connecting')
+        await pgClient.connect()
+        console.log('connected')
+
+        const createSchemaQuery = `
+      DO
+      $do$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT FROM pg_catalog.pg_namespace WHERE nspname = '${schemaName}'
+        ) THEN
+          CREATE SCHEMA ${schemaName};
+        END IF;
+      END
+      $do$
+    `
+        await pgClient.query(createSchemaQuery)
+        console.log('done creating schema')
+    } catch (e) {
+        console.error('error creating schema: ', e)
+        error = e
+    } finally {
+        await pgClient.end()
+    }
+
+    if (error) {
+        throw error
+    }
+}
+
+const createNotificationServiceDbUser = async () => {
+    console.log('creating notification service db user')
+
+    const pgClient = new Client({
+        host: env.DB_HOST,
+        database: 'postgres',
+        password: env.DB_ROOT_PASSWORD,
+        user: env.DB_ROOT_USER,
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    })
+
+    let error
+
+    try {
+        console.log('connecting')
+        await pgClient.connect()
+        console.log('connected')
+
+        // create user if not exists
+        console.log('creating user')
+
+        const createUserQuery = `
+      DO
+      $do$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT FROM pg_catalog.pg_user WHERE usename = '${env.DB_NOTIFICATION_SERVICE_USER}'
+        ) THEN
+          CREATE USER ${env.DB_NOTIFICATION_SERVICE_USER} WITH PASSWORD '${env.DB_NOTIFICATION_SERVICE_USER_PASSWORD}';
+        END IF;
+      END
+      $do$
+    `
+
+        await pgClient.query(createUserQuery)
+        console.log('created user')
+
+        try {
+            console.log('creating database')
+            const createDatabaseQuery = `CREATE DATABASE ${env.DB_DATABASE};`
+            await pgClient.query(createDatabaseQuery)
+            console.log('created database')
+        } catch (e) {
+            if (isDBAlreadyExistsError(e)) {
+                console.log('database already exists')
+            } else {
+                console.error('error creating database: ', e)
+                throw e
+            }
+        }
+
+        console.log('granting create schema to user')
+
+        await pgClient.query('SELECT pg_advisory_lock(1);') // 1 is an arbitrary lock ID
+        const grantCreateSchemaQuery = `
+      GRANT CREATE ON DATABASE ${env.DB_DATABASE} TO ${env.DB_NOTIFICATION_SERVICE_USER};
+    `
+
+        // run the query above with 5 retry attempts:
+        const executeCreateQuery = async () => {
+            let attempt = 0
+            let error
+            while (attempt < 5) {
+                try {
+                    await pgClient.query(grantCreateSchemaQuery)
+                    break
+                } catch (e) {
+                    console.warn(`error executing create query attempt: ${attempt}`, e)
+                    error = e
+                    attempt++
+                }
+            }
+            if (error) {
+                throw error
+            }
+        }
+
+        await executeCreateQuery()
+
+        console.log('done creating user')
+    } catch (e) {
+        error = e
+    } finally {
+        await pgClient.query('SELECT pg_advisory_unlock(1);')
+        await pgClient.end()
+    }
+
+    if (error) {
+        throw error
+    }
+}
+
+export const run = async () => {
+    await createNotificationServiceDbUser()
+    await createSchema()
+
+    console.log('success')
+}
