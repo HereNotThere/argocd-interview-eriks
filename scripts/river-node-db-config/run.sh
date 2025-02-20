76059decd181a8279df9f0edd4c1b2988bf63c15
Kerem Kazan <kerem.kazan@gmail.com>
2025-02-20 10:16:54 -0500

    added db config script for river nodes

---
+++ scripts/river-node-db-config/run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -eo pipefail
+
+
+init() {
+  echo "Running init-db"
+  yarn install
+  yarn db:init
+}
+
+migrate() {
+  echo "Running migrate-db"
+
+  if [ -z "$TARGET_DB_HOST" ]; then
+      echo "TARGET_DB_HOST is not set. Exiting."
+      exit 1
+  fi 
+
+  if [ -z "$TARGET_DB_DATABASE" ]; then
+      echo "TARGET_DB_DATABASE is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$TARGET_DB_APP_USER" ]; then
+      echo "TARGET_DB_APP_USER is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$TARGET_DB_APP_PASSWORD" ]; then
+      echo "TARGET_DB_APP_PASSWORD is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$SOURCE_DB_HOST" ]; then
+      echo "SOURCE_DB_HOST is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$SOURCE_DB_DATABASE" ]; then
+      echo "SOURCE_DB_DATABASE is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$SOURCE_DB_USER" ]; then
+      echo "SOURCE_DB_USER is not set. Exiting."
+      exit 1
+  fi
+
+  if [ -z "$SOURCE_DB_PASSWORD" ]; then
+      echo "SOURCE_DB_PASSWORD is not set. Exiting."
+      exit 1
+  fi
+
+  export PGPASSWORD=$SOURCE_DB_PASSWORD
+  pg_dump -h $SOURCE_DB_HOST -U $SOURCE_DB_USER -d $SOURCE_DB_DATABASE -F d -j 4 -f /tmp/dump
+
+  export PGPASSWORD=$TARGET_DB_APP_PASSWORD
+  pg_restore -h $TARGET_DB_HOST -U $TARGET_DB_APP_USER -d $TARGET_DB_DATABASE -j 4 -O -x -F d /tmp/dump
+
+  echo "Finished migrate-db"
+}
+
+init
+migrate
---
+++ scripts/river-node-db-config/src/index.ts
@@ -0,0 +1,122 @@
+import { Client, DatabaseError } from 'pg'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    TARGET_DB_HOST: z.string().min(1),
+    TARGET_DB_DATABASE: z.string().min(1),
+    TARGET_DB_ROOT_USER: z.string().min(1),
+    TARGET_DB_ROOT_PASSWORD: z.string().min(1),
+    TARGET_DB_APP_USER: z.string().min(1),
+    TARGET_DB_APP_PASSWORD: z.string().min(1),
+})
+
+const env = envSchema.parse(process.env)
+
+function isDBAlreadyExistsError(e: unknown) {
+    return e instanceof DatabaseError && (e.code == '42P04' || e.code == '23505')
+}
+
+const createDbUser = async () => {
+    console.log('creating notification service db user')
+
+    const pgClient = new Client({
+        host: env.TARGET_DB_HOST,
+        database: 'postgres',
+        password: env.TARGET_DB_ROOT_PASSWORD,
+        user: env.TARGET_DB_ROOT_USER,
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    })
+
+    let error
+
+    try {
+        console.log('connecting')
+        await pgClient.connect()
+        console.log('connected')
+
+        // create user if not exists
+        console.log('creating user')
+
+        const createUserQuery = `
+      DO
+      $do$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT FROM pg_catalog.pg_user WHERE usename = '${env.TARGET_DB_APP_USER}'
+        ) THEN
+          CREATE USER ${env.TARGET_DB_APP_USER} WITH PASSWORD '${env.TARGET_DB_APP_PASSWORD}';
+        END IF;
+      END
+      $do$
+    `
+
+        await pgClient.query(createUserQuery)
+        console.log('created user')
+
+        try {
+            console.log('creating database')
+            const createDatabaseQuery = `CREATE DATABASE ${env.TARGET_DB_DATABASE};`
+            await pgClient.query(createDatabaseQuery)
+            console.log('created database')
+        } catch (e) {
+            if (isDBAlreadyExistsError(e)) {
+                console.log('database already exists')
+            } else {
+                console.error('error creating database: ', e)
+                throw e
+            }
+        }
+
+        console.log('granting create schema to user')
+
+        await pgClient.query('SELECT pg_advisory_lock(1);') // 1 is an arbitrary lock ID
+        const grantCreateSchemaQuery = `
+      GRANT CREATE ON DATABASE ${env.TARGET_DB_DATABASE} TO ${env.TARGET_DB_APP_USER};
+    `
+
+        // run the query above with 5 retry attempts:
+        const executeCreateQuery = async () => {
+            let attempt = 0
+            let error
+            while (attempt < 5) {
+                try {
+                    await pgClient.query(grantCreateSchemaQuery)
+                    break
+                } catch (e) {
+                    console.warn(`error executing create query attempt: ${attempt}`, e)
+                    error = e
+                    attempt++
+                }
+            }
+            if (error) {
+                throw error
+            }
+        }
+
+        await executeCreateQuery()
+
+        console.log('done creating user')
+    } catch (e) {
+        error = e
+    } finally {
+        await pgClient.query('SELECT pg_advisory_unlock(1);')
+        await pgClient.end()
+    }
+
+    if (error) {
+        throw error
+    }
+}
+
+export const run = async () => {
+    await createDbUser()
+    console.log('success')
+}
+
+run().catch((e) => {
+    console.error('error: ', e)
+    process.exit(1)
+})
